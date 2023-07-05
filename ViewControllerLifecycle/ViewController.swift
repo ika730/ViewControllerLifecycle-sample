61050e03a865b7dd297708fddae6b00a37f05ca7
Nakagawa Kosuke <>
2023-07-05 20:13:31 +0900

    微調整

--- ViewControllerLifecycle/ViewController.swift
+++ ViewControllerLifecycle/ViewController.swift
@@ -20,7 +20,16 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         print("VC1 viewDiddAppear")
     }
-
+    override func viewWillDisappear(_ animated: Bool) {
+        print("VC1 viewWillDisappear")
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        print("VC1 viewDidDisappear")
+    }
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        let destinationVC = segue.destination as! ViewController2
+//        destinationVC.VC2label.text = "Hello"
+//    }
 
 }
 
--- ViewControllerLifecycle/ViewController2.swift
+++ ViewControllerLifecycle/ViewController2.swift
@@ -8,7 +8,9 @@
 import UIKit
 
 class ViewController2: UIViewController {
+    
 
+    @IBOutlet weak var VC2label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,7 +22,9 @@ class ViewController2: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         print("VC2 viewDiddAppear")
     }
-    
+    override func viewDidDisappear(_ animated: Bool) {
+        print("VC2 viewDidDisappear")
+    }
 
     @IBAction func back(_ sender: UIButton) {
         dismiss(animated: true)
